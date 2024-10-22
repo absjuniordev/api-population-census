2991ded2df41a9b99e9bf3d8c5e5d24e1e30c9ec
Arnaldo Junior <abs.junnior@hotmail.com>
2024-10-22 12:05:47 -0300

    Inicio do projeto

---
+++ src/test/java/com/absjunior/ConsumerCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.absjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
