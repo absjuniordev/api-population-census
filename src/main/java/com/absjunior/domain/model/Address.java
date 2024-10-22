20c3f6457282c12cf4aa6f973e50170d191bf9ca
Arnaldo Junior <abs.junnior@hotmail.com>
2024-10-22 17:01:32 -0300

    Ajustes

--- src/main/java/com/absjunior/domain/model/Address.java
+++ src/main/java/com/absjunior/domain/model/Address.java
@@ -5,126 +5,97 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Address {
-	
+
 	@Id
-    private String cep;
-    private String logradouro;
-    private String complemento;
-    private String unidade;
-    private String bairro;
-    private String localidade;
-    private String uf;
-    private String estado;
-    private String regiao;
-    private String ibge;
-    private String gia;
-    private String ddd;
-    private String siafi;
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String localidade;
+	private String uf;
+	private String ibge;
+	private String gia;
+	private String ddd;
+	private String siafi;
 
-    // Getters e Setters
+	public String getCep() {
+		return cep;
+	}
 
-    public String getCep() {
-        return cep;
-    }
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
 
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
+	public String getLogradouro() {
+		return logradouro;
+	}
 
-    public String getLogradouro() {
-        return logradouro;
-    }
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
 
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
+	public String getComplemento() {
+		return complemento;
+	}
 
-    public String getComplemento() {
-        return complemento;
-    }
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
 
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
+	public String getBairro() {
+		return bairro;
+	}
 
-    public String getUnidade() {
-        return unidade;
-    }
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
 
-    public void setUnidade(String unidade) {
-        this.unidade = unidade;
-    }
+	public String getLocalidade() {
+		return localidade;
+	}
 
-    public String getBairro() {
-        return bairro;
-    }
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
 
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
+	public String getUf() {
+		return uf;
+	}
 
-    public String getLocalidade() {
-        return localidade;
-    }
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 
-    public void setLocalidade(String localidade) {
-        this.localidade = localidade;
-    }
+	public String getIbge() {
+		return ibge;
+	}
 
-    public String getUf() {
-        return uf;
-    }
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
 
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
+	public String getGia() {
+		return gia;
+	}
 
-    public String getEstado() {
-        return estado;
-    }
+	public void setGia(String gia) {
+		this.gia = gia;
+	}
 
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
+	public String getDdd() {
+		return ddd;
+	}
 
-    public String getRegiao() {
-        return regiao;
-    }
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
 
-    public void setRegiao(String regiao) {
-        this.regiao = regiao;
-    }
+	public String getSiafi() {
+		return siafi;
+	}
 
-    public String getIbge() {
-        return ibge;
-    }
+	public void setSiafi(String siafi) {
+		this.siafi = siafi;
+	}
 
-    public void setIbge(String ibge) {
-        this.ibge = ibge;
-    }
-
-    public String getGia() {
-        return gia;
-    }
-
-    public void setGia(String gia) {
-        this.gia = gia;
-    }
-
-    public String getDdd() {
-        return ddd;
-    }
-
-    public void setDdd(String ddd) {
-        this.ddd = ddd;
-    }
-
-    public String getSiafi() {
-        return siafi;
-    }
-
-    public void setSiafi(String siafi) {
-        this.siafi = siafi;
-    }
 }
-
